e8333973d4bd44e0e6c22d3db42a20475920b7ca
Zhasik <zhasik@gmail.com>
2020-01-06 18:35:41 +0600

    Change listener to recieve topic and data separetly

---
+++ dist/mqtt-pubsub.d.ts
@@ -0,0 +1,36 @@
+import { PubSubEngine } from 'graphql-subscriptions/dist/pubsub-engine';
+import { Client, ISubscriptionGrant, IClientPublishOptions, IClientSubscribeOptions } from 'mqtt';
+export interface PubSubMQTTOptions {
+    brokerUrl?: string;
+    client?: Client;
+    connectionListener?: (err: Error) => void;
+    publishOptions?: PublishOptionsResolver;
+    subscribeOptions?: SubscribeOptionsResolver;
+    onMQTTSubscribe?: (id: number, granted: ISubscriptionGrant[]) => void;
+    triggerTransform?: TriggerTransform;
+    parseMessageWithEncoding?: string;
+}
+export declare class MQTTPubSub implements PubSubEngine {
+    private triggerTransform;
+    private onMQTTSubscribe;
+    private subscribeOptionsResolver;
+    private publishOptionsResolver;
+    private mqttConnection;
+    private subscriptionMap;
+    private subsRefsMap;
+    private currentSubscriptionId;
+    private parseMessageWithEncoding;
+    private static matches;
+    constructor(options?: PubSubMQTTOptions);
+    publish(trigger: string, payload: any): boolean;
+    subscribe(trigger: string, onMessage: Function, options?: Object): Promise<number>;
+    unsubscribe(subId: number): void;
+    asyncIterator<T>(triggers: string | string[]): AsyncIterator<T>;
+    private onMessage;
+}
+export declare type Path = Array<string | number>;
+export declare type Trigger = string | Path;
+export declare type TriggerTransform = (trigger: Trigger, channelOptions?: Object) => string;
+export declare type SubscribeOptionsResolver = (trigger: Trigger, channelOptions?: Object) => Promise<IClientSubscribeOptions>;
+export declare type PublishOptionsResolver = (trigger: Trigger, payload: any) => Promise<IClientPublishOptions>;
+export declare type SubscribeHandler = (id: number, granted: ISubscriptionGrant[]) => void;
---
+++ dist/pubsub-async-iterator.d.ts
@@ -0,0 +1,23 @@
+import { $$asyncIterator } from 'iterall';
+import { PubSubEngine } from 'graphql-subscriptions/dist/pubsub-engine';
+export declare class PubSubAsyncIterator<T> implements AsyncIterator<T> {
+    private pullQueue;
+    private pushQueue;
+    private eventsArray;
+    private allSubscribed;
+    private listening;
+    private pubsub;
+    constructor(pubsub: PubSubEngine, eventNames: string | string[]);
+    next(): Promise<IteratorResult<any>>;
+    return(): Promise<{
+        value: any;
+        done: boolean;
+    }>;
+    throw(error: any): Promise<never>;
+    [$$asyncIterator](): this;
+    private pushValue;
+    private pullValue;
+    private emptyQueue;
+    private subscribeAll;
+    private unsubscribeAll;
+}
